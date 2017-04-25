a9447a2ec10a24ad2a70d8f0f07db43a0b81edbe
Thomas Couchoud <zerderr@gmail.com>
2017-04-25 16:09:18 +0200

    refactor

--- src/main/java/polytech/tours/di/parallel/tsp/Launcher.java
+++ src/main/java/polytech/tours/di/parallel/tsp/Launcher.java
@@ -17,57 +17,77 @@ public class Launcher
 	
 	/**
 	 * @param args:
-	 *           0: the file (path included) with he configuration settings
+	 *           0: the file (path included) with the configuration settings
 	 */
 	public static void main(String[] args)
 	{
-		ArrayList<String> results = new ArrayList<>();
-
 		String folder = "./config/graph6/";
 		String files[] = {"serial", "20-20-1", "1000-200-1", "1000-500-1", "20-20-3"};
-
-		for(String file : files)
+		
+		if(args.length > 0)
 		{
-			file += ".xml";
-			System.out.println("Processing file " + folder + file);
-
-			//read properties
 			Properties config = new Properties();
 			try
 			{
-				if(args.length > 0)
-					config.loadFromXML(new FileInputStream(args[0]));
-				else
-					//config.loadFromXML(new FileInputStream("./config/Example_config.xml"));
-					config.loadFromXML(new FileInputStream(folder + file));
+				config.loadFromXML(new FileInputStream(args[0]));
 			}
 			catch(IOException e)
 			{
 				e.printStackTrace();
+				System.exit(1);
 			}
-
-			//dynamically load the algorithm class
-			Algorithm algorithm = null;
-			try
-			{
-				Class<?> c = Class.forName(config.getProperty("algorithm"));
-				algorithm = (Algorithm) c.newInstance();
-			}
-			catch(ClassNotFoundException | InstantiationException | IllegalAccessException e)
-			{
-				e.printStackTrace();
-			}
-
-			ArrayList<Solution> solutions = new ArrayList<>();
-			for(int i = 1; i <= 10; i++)
-			{
-				System.out.print(i + "/10 ==>\t\t");
-				solutions.add(algorithm.run(config));
-			}
-			String result = solutions.stream().map(solution -> "" + solution.getOF()).collect(Collectors.joining("\t"));
-			results.add(result);
-			System.out.println(result + "\n");
+			System.out.println(runAlgorithm(config));
 		}
-		System.out.println("\n\n" + results.stream().collect(Collectors.joining("\n")));
+		else
+		{
+			ArrayList<String> results = new ArrayList<>();
+			
+			for(String file : files)
+			{
+				file += ".xml";
+				System.out.println("Processing file " + folder + file);
+				
+				//read properties
+				Properties config = new Properties();
+				try
+				{
+					//config.loadFromXML(new FileInputStream("./config/Example_config.xml"));
+					config.loadFromXML(new FileInputStream(folder + file));
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+					System.exit(1);
+				}
+				
+				String result = runAlgorithm(config);
+				results.add(result);
+				System.out.println(result + "\n");
+			}
+			System.out.println("\n\n" + results.stream().collect(Collectors.joining("\n")));
+		}
+	}
+	
+	private static String runAlgorithm(Properties config)
+	{
+		//dynamically load the algorithm class
+		Algorithm algorithm = null;
+		try
+		{
+			Class<?> c = Class.forName(config.getProperty("algorithm"));
+			algorithm = (Algorithm) c.newInstance();
+		}
+		catch(ClassNotFoundException | InstantiationException | IllegalAccessException e)
+		{
+			e.printStackTrace();
+		}
+		
+		ArrayList<Solution> solutions = new ArrayList<>();
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.print(i + "/10 ==>\t\t");
+			solutions.add(algorithm.run(config));
+		}
+		return solutions.stream().map(solution -> "" + solution.getOF()).collect(Collectors.joining("\t"));
 	}
 }
